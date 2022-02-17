20ed49e0d408ce8197439476473a1f31f37575a4
Alex Poirier <alexmatthewpoirier@gmail.com>
2022-02-16 21:04:29 -0500

    Add project files.

---
+++ SalesWebApi/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebApi.Models {
+    public class AppDbContext : DbContext {
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+    }
+}
